8150b675f54704e44c521044763309102e31ab52
Carlos Rodriguez <rodriguezcj22@gmail.com>
2021-05-07 08:42:48 -0400

    Updated version to get help with deselecting current highlighted cell
    Updated version to get help with deselecting the cell after the stop button has been pressed.

---
+++ WorshipPads/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func greeting() {
+    
+    print("hello")
+    var myName = "Angela"
+    
+    print(myName)
+    
+    func greeting2() {
+        print("Hey")
+    }
+}
+
+func greeting3() {
+    
+}
+
+greeting()
